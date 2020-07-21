6ce3b29cc9a7fb9085ee167016e1d7b709dce7ac
cliftondavies <clifton.davies@outlook.com>
2020-07-21 16:43:38 +0100

    Add user_id as foreign key to posts

--- db/migrate/20200721153708_add_fields_to_users.rb
+++ db/migrate/20200721153708_add_fields_to_users.rb
@@ -2,6 +2,6 @@ class AddFieldsToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string
     add_column :users, :username, :string
-    add_index :users, :username
+    add_index :users, :username, unique: true
   end
 end
